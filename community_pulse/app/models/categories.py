f3f19c579047e44504de3096afc116af5820a7e7
Кирилл Майоров <k.mayorov@megaplan.ru>
2025-03-27 20:34:00 +0300

    Работа в классе

--- community_pulse/app/models/categories.py
+++ community_pulse/app/models/categories.py
@@ -7,7 +7,6 @@ class Category(db.Model):
     __tablename__ = 'categories'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False)
-    question: Mapped["Question"] = relationship(back_populates="questions")
 
     def __repr__(self):
         return f'Category: {self.name}'
